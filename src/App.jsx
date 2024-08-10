ee8bfc96f0a301a39d1c20b100ea7de16e3c870d
Virender Prasad <virender288@gmail.com>
2024-08-10 17:27:51 +0530

    updated the backgimage with input tab and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+import backgroundimg from './assets/backgroundimg.jpg' 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <div
+          className="h-screen w-full bg-cover bg-center flex flex-col items-center justify-start p-4"
+          style={{ backgroundImage: `url(${backgroundimg})` }}
+        >
+          <h1 className="text-yellow-500 text-4xl font-bold mt-8 mb-6 p-4 text-center">
+            Weather Forecast
+          </h1>
+          <div className="flex w-full max-w-2xl space-x-4 mb-8 items-center">
+            <input
+              type="text"
+              placeholder="Enter City or Country Name Eg:- USA,Japan,Delhi,San Francisco.."
+              className="flex-1 h-12 p-3 border border-gray-300 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <button className="h-12 px-6 bg-orange-500 text-white rounded-lg shadow-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500">
+              Submit
+            </button>
+          </div>
+        </div>
     </>
   )
 }
